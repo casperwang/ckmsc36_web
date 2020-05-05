e3322b07e031f785bde46d17924584ce1c556f9d
xSeanliux <zhxnliu@gmail.com>
2020-05-05 16:11:53 +0800

    New animation for back btn for group paeg

--- js/team.js
+++ js/team.js
@@ -1,19 +1,53 @@
 console.log("AAAA");
 
 function checkButton(){
-	$("bigText").on({
+	$("#backButton").on({
 	    mouseenter: function() {
-	    	
+	    	console.log("Jizz");
+	    	anime.timeline()
+	    	.add({
+	    		targets: "#cursor",
+	    		translateX: [0, 120],
+	    		duration: 150,
+	    		easing: "easeInQuad"
+	    	})
+	    	.add({
+	    		targets: "#bannerText .letter",
+	    		opacity: [0, 100],
+	    		delay: function(el, i, l){
+	    			return i * 20;
+	    		},
+	    		duration: 75,
+	    		offset: "-=10",
+	    		easing: "easeInQuad"
+	    	});
 	    },
 	    mouseleave: function() {
-	    	
+	    	anime.timeline()
+	    	.add({
+	    		targets: "#bannerText .letter",
+	    		opacity: [100, 0],
+	    		delay: function(el, i, l){
+	    			return -i * 20;
+	    		},
+	    		duration: 75,
+	    		easing: "easeOutQuad"
+	    	})
+	    	.add({
+	    		targets: "#cursor",
+	    		translateX: [120, 0],
+	    		duration: 150,
+	    		easing: "easeOutQuad"
+	    	});
+	    },
+	    click: function(){
+	    	window.location.href = "../../menupage.html";
 	    }
 	});
 }
 
 
 $(document).ready(function(){ //TODO - load the text with animations
-	console.log("JJJJ");
 	checkButton();
 });
 
