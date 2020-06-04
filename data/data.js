9c7803fcaea5d116deac94e8fdf06f2d5fff01f3
casperwang <casperwang2014@gmail.com>
2020-06-04 13:58:32 +0800

    update team

--- data/data.js
+++ data/data.js
@@ -1,12 +1,36 @@
 var data = {
-    "inform" : [{
+    "math": [{
+        "title": "Test",
+    }],
+    "inform": [{
         "title": "Test",
         "author": [{
             "name": "辣椒頭哥哥",
+            "photo": "",
             "class": 226,
-            "number": 16
-        "專題簡介":
-        ""
-        }]
+            "number": 16,
+            "desciption": "辣椒頭哥哥好辣"
+        },{
+            "name": "辣椒頭哥哥",
+            "photo": "",
+            "class": 226,
+            "number": 16,
+            "desciption": "辣椒頭哥哥好辣"
+        },],
+        "summary": "兵者，詭道也。故能而示之不能，用而示之不用，近而示之遠，遠而示之近。利而誘之，亂而取之，實而備之，強而避之，怒而撓之，卑而驕之，佚而勞之，親而離之。攻其無備，出其不意。此兵家之勝，不可先傳也。",
+        "pdf": "I3",
+        "video": ""
+    }],
+    "chem": [{
+        "title": "Test",
+    }],
+    "earths": [{
+        "title": "Test",
+    }],
+    "phys": [{
+        "title": "Test",
+    }],
+    "bio": [{
+        "title": "Test",
     }]
 };
--- js/team.js
+++ js/team.js
@@ -1,8 +1,11 @@
 function updateContent() {
-	var obj = data[curTeam.sub][curTeam.id]
-	$('#project_title').html(obj.title)
-	$('#project_author').html(obj.author)
-	console.log(obj)
+	var obj = data[curTeam.sub][curTeam.id];
+	console.log(obj.author[0].name);
+	$('#project_title').html(obj.title);
+	$('#project_author').html(obj.author.length);
+	$('#author_1').html(obj.author[0].name);
+	$('#author_1_content').html(obj.author[0].desciption);
+	$('#author_2').html(obj.author[1].name);
 }
 
 $(document).ready(function(){ //TODO - load the text with animations
